8ab8504d3d0e8e4a0696d8189a3c22d7333dae98
JSConF <arare30030@naver.com>
2023-06-06 20:22:20 +0900

    계좌등록 서비스, 컨트롤러 테스트 완료

--- src/main/java/shop/jsconf/bank/config/dummy/DummyObject.java
+++ src/main/java/shop/jsconf/bank/config/dummy/DummyObject.java
@@ -1,6 +1,7 @@
 package shop.jsconf.bank.config.dummy;
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import shop.jsconf.bank.domain.account.Account;
 import shop.jsconf.bank.domain.user.User;
 import shop.jsconf.bank.domain.user.UserEnum;
 
@@ -17,7 +18,7 @@ public class DummyObject {
                 .fullname(fullname)
                 .role(UserEnum.CUSTOMER)
                 .build();
-    };
+    }
 
     protected User newMockUser(Long id, String username, String fullname){
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
@@ -32,5 +33,26 @@ public class DummyObject {
                 .createdAt(LocalDateTime.now())
                 .updatedAt(LocalDateTime.now())
                 .build();
-    };
+    }
+
+    protected Account newAccount(Long number, User user) {
+        return Account.builder()
+                .number(number)
+                .password(1234L)
+                .balance(1000L)
+                .user(user)
+                .build();
+    }
+
+    protected Account newMockAccount(Long id, Long number, Long balance, User user) {
+        return Account.builder()
+                .id(id)
+                .number(number)
+                .password(1234L)
+                .balance(balance)
+                .user(user)
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+    }
 }
---
+++ src/test/java/shop/jsconf/bank/service/AccountServiceTest.java
@@ -0,0 +1,69 @@
+package shop.jsconf.bank.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import shop.jsconf.bank.config.dummy.DummyObject;
+import shop.jsconf.bank.domain.account.Account;
+import shop.jsconf.bank.domain.account.AccountRepository;
+import shop.jsconf.bank.domain.user.User;
+import shop.jsconf.bank.domain.user.UserRepository;
+import shop.jsconf.bank.dto.account.AccountReqDto;
+import shop.jsconf.bank.dto.account.AccountRespDto;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static shop.jsconf.bank.dto.account.AccountReqDto.*;
+import static shop.jsconf.bank.dto.account.AccountRespDto.*;
+
+@ExtendWith(MockitoExtension.class)
+public class AccountServiceTest extends DummyObject {
+
+    @InjectMocks // 모든 Mock들이 InjectMocks 로 주입됨
+    private AccountService accountService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private AccountRepository accountRepository;
+
+    @Spy // 진짜 객체를  InjectMocks 에 주입한다.
+    private ObjectMapper om;
+
+    @Test
+    public void create_account_test() throws Exception {
+        // given
+        Long userId = 1L;
+
+        AccountSaveReqDto accountSaveReqDto = new AccountSaveReqDto();
+        accountSaveReqDto.setNumber(1111L);
+        accountSaveReqDto.setPassword(1234L);
+
+        // stub 1
+        User ssar = newMockUser(1L, "ssar", "쌀");
+        when(userRepository.findById(any())).thenReturn(Optional.of(ssar));
+
+        // stub 2
+        when(accountRepository.findByNumber(any())).thenReturn(Optional.empty());
+
+        // stub 3
+        Account ssarAccount = newMockAccount(1L, 1111L, 1000L, ssar);
+        when(accountRepository.save(any())).thenReturn(ssarAccount);
+
+        // when
+        AccountSaveRespDto accountSaveRespDto = accountService.accountCreate(accountSaveReqDto, userId);
+        String responseBody = om.writeValueAsString(accountSaveRespDto);
+        System.out.println("테스트 : " + responseBody);
+
+        // then
+        assertThat(accountSaveRespDto.getNumber()).isEqualTo(1111);
+    }
+}
